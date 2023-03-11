50b4c07e9b54e3ca351f6a0f50b308c1b2b531b2
JoaquinC539 <cjoaquinangel@gmail.com>
2023-03-11 09:08:07 -0600

    Moved methods to class

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,16 +1,17 @@
 const express=require('express');
 import { Connection } from '../config/dbCon';
-import { getUser,getAll,newUser,editUser,deleteUser } from '../controllers/controller';
+import { Controller } from '../controllers/controller';
+const controller=new Controller();
 const connection = new Connection();
 const pool=connection.pool;
 export class Router{
     router=express.Router();
 
     constructor(){
-        this.router.get("/user/:id",getUser);
-        this.router.get("/all", getAll );
-        this.router.post("/new",newUser );
-        this.router.put("/update",editUser );
-        this.router.delete("/delete", deleteUser);
+        this.router.get("/user/:id",controller.getUser);
+        this.router.get("/all",controller.getAll );
+        this.router.post("/new",controller.newUser );
+        this.router.put("/update",controller.editUser );
+        this.router.delete("/delete", controller.deleteUser);
     }
     }
